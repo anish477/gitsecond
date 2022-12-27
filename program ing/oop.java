7a1f9bee46dd1a539d4a4ce39830cb377aa178ae
Anish K.T. Sharma <np03cs4a220237@heraldcollege.edu.np>
2022-12-27 09:54:36 +0545

    worshop5

---
+++ program ing/oop.java
@@ -0,0 +1,18 @@
+public class Pen{
+
+String color;
+String type;
+   
+   public void  write() {
+       System.out.println("writing something")
+   }
+}
+public class OOPS {
+public static void main (String args[]){
+    Pen pen1 = new pen();
+    pen1.color="blue";
+    pen1.type="gel";
+    pen1.write();
+}
+
+}
